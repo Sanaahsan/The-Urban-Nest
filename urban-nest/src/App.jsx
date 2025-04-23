68f945fea4a48b12a02533189cfa8c4b8801331d
Sana Ahsan <sanaahsanmallick@gmail.com>
2025-04-24 01:47:39 +0530

    Header Added

--- urban-nest/src/App.jsx
+++ urban-nest/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 
 import Header from "./components/Header";
+import About from "./components/About";
 
 
 function App() {
    return (
     <div className="w-full overflow-hidden">
       <Header/>
+      <About/>
     </div>
     
     
