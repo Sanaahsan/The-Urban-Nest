edf6239b6b640d03c6771ee2257c723562b32e98
Sana Ahsan <sanaahsanmallick@gmail.com>
2025-04-25 02:35:07 +0530

    Projects page added

--- urban-nest/src/App.jsx
+++ urban-nest/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Header from "./components/Header";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="w-full overflow-hidden">
       <Header/>
       <About/>
+      <Projects/>
     </div>
     
     
