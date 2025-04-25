a73806b8830df329e20dd4e2fddde73508507bb5
Sana Ahsan <sanaahsanmallick@gmail.com>
2025-04-26 01:50:08 +0530

    Contact page Added

--- urban-nest/src/App.jsx
+++ urban-nest/src/App.jsx
@@ -4,15 +4,20 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
    return (
     <div className="w-full overflow-hidden">
+    <ToastContainer></ToastContainer>
       <Header/>
       <About/>
       <Projects/>
       <Testimonials/>
+      <Contact/>
     </div>
     
     
